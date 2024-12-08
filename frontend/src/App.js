9cd9fb0284deb2160ecfea698aef7653a2055470
yifei-yao <yaoyifei0@gmail.com>
2024-12-07 20:44:03 -0500

    Add 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Routes>
+        {/* Homepage route */}
+        <Route path="/" element={<Home />} />
+
+        {/* Catch-all route for 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
       <h1>Welcome Home</h1>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
     </div>
   );
 }
