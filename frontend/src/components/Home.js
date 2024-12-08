db814300d77d433ca4eaa6ed23ad144122781122
yifei-yao <yaoyifei0@gmail.com>
2024-12-08 01:43:57 -0500

    Refactor

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+      <p>
+        <a href="/register">Register</a> | <a href="/login">Login</a>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
